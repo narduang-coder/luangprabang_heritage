1a9a1e578c290bc36559c52e2d5a8681f52f39c8
nar <you@example.com>
2026-06-09 22:02:31 +0700

    ບ

--- config/database.php
+++ config/database.php
@@ -1,10 +1,12 @@
 <?php
-$server = "localhost";
-$user = "root";
-$password = "";
-$db_name = "luangprabang_heritage";
-$connect = mysqli_connect($server, $user, $password, $db_name);
-mysqli_set_charset($connect, "utf8");
+// config/database.php
+$host = getenv("MYSQLHOST");
+$port = getenv("MYSQLPORT");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
+
+$connect = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$connect) {
     die("ການເຊື່ອມຕໍ່ຖານຂໍ້ມູນລົ້ມເຫຼວ: " . mysqli_connect_error());
