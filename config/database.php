9adf1306e57b54a33e35673732e2a346ac508433
nar <you@example.com>
2026-06-09 08:04:26 +0700

    pp

--- config/database.php
+++ config/database.php
@@ -1,13 +1,10 @@
 <?php
-// config/database.php
-$server = 'mysql-1kri.railway.internal';  // หรือใช้ 'mysql-1kri.up.railway.app'
-$user = 'root';
-$password = 'tTFPdYKTjSRhiQJugkkJqtYVnMPHDQNu';  // ใช้ตัวที่ถูกต้อง
-$db_name = 'railway';
-$port = 3306;
-
-$connect = mysqli_connect($server, $user, $password, $db_name, $port);
-mysqli_set_charset($connect, "utf8mb4");
+$server = "localhost";
+$user = "root";
+$password = "";
+$db_name = "luangprabang_heritage";
+$connect = mysqli_connect($server, $user, $password, $db_name);
+mysqli_set_charset($connect, "utf8");
 
 if (!$connect) {
     die("ການເຊື່ອມຕໍ່ຖານຂໍ້ມູນລົ້ມເຫຼວ: " . mysqli_connect_error());
